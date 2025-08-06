b1e72f2a3d93c3d48bdb04deff3a0a62968633d6
Yash Lala <yashlala@gmail.com>
2025-08-07 01:55:32 +0200

    fix: lat-breakdown wrapper is EPFL-specific.
    Now, it should work even when not using the EPFL clusters.

--- mage-kernel/apps/sequential-read-lat-breakdown/run-benchmark.zsh
+++ mage-kernel/apps/sequential-read-lat-breakdown/run-benchmark.zsh
@@ -7,9 +7,7 @@ fi
 source $MIND_ROOT/scripts/config.sh
 cd $MIND_ROOT/apps/sequential-read-lat-breakdown
 
-# TODO uncomment before submission to AE
-#bench_branch='sosp_ae_latency_breakdown'
-bench_branch='sosp_ae_lat_breakdown_yale'
+bench_branch='sosp_ae_lat_breakdown'
 
 echo "Execute this on the VM host only!"
 
@@ -17,14 +15,15 @@ if [[ "$(git rev-parse --abbrev-ref HEAD)" != $bench_branch ]]; then
         echo "Error: VM Host is not on $bench_branch branch!" >/dev/stderr
         exit 1
 fi
+echo "Check passed: VM host is on $bench_branch branch."
 
 echo "Before running this, please make sure the Compute Node is on $bench_branch branch!" 
-# TODO(yash): Don't hardcode this anymore! Should be run in $MIND_ROOT!
-#vm_branch="$(ssh $cn_control_sshname zsh -c 'cd /home/sslee/rfbs && git rev-parse --abbrev-ref HEAD' 2>/dev/null)"
-#if [[ $vm_branch != $bench_branch ]]; then
-#        echo "Error: Compute Node is not on $bench_branch branch!" >/dev/stderr
-#        exit 1
-#fi
+vm_branch="$(ssh $cn_control_sshname 'cd $MIND_ROOT && git rev-parse --abbrev-ref HEAD' 2>/dev/null)"
+if [[ $vm_branch != $bench_branch ]]; then
+        echo "Error: Compute Node is not on $bench_branch branch!" >/dev/stderr
+        exit 1
+fi
+echo "Check passed: Compute Node VM is on $bench_branch branch."
 
 # COMPILE THE BENCHMARK APPLICATION, COMPILE KERNEL, RUN TESTS
 echo "Running tests!"
