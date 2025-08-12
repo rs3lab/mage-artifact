a99fe78470738a41399a7fe171ccc9b65f1a6673
Yash Lala <yashlala@gmail.com>
2025-08-12 04:16:22 +0200

    util: sketch out bench script for Pan's memcached.

---
+++ mage-kernel/apps/memcached/run-benchmark.zsh
@@ -0,0 +1,62 @@
+#!/bin/zsh -e
+
+if [[ -z $MIND_ROOT ]]; then
+        echo '$MIND_ROOT not set!' >/dev/stderr
+        exit 1
+fi
+source $MIND_ROOT/scripts/config.sh
+cd $MIND_ROOT/apps/memcached
+
+echo "Execute this on the VM host only!"
+
+function set-params () {
+	local lmem_mib=$1
+	local sleep_time=$2
+
+	# rebuild application w/ new params
+	ssh $cn_control_sshname "zsh sed -i 's/#define SLEEP_TIME .*/#define SLEEP_TIME $sleep_time/' $MIND_ROOT/apps/memcached/src/test_config.h"
+	ssh $cn_control_sshname "make -C $MIND_ROOT/apps/memcached/"
+
+	# rebuild kernel w/ new params
+	ssh $cn_control_sshname set-params 'memcached' $cn $bs $lmem_mib 0.8
+	manager cn allonly
+	sleep 1s
+
+	# reset the cluster (updating the kernel as well)
+	reset-fbs
+	sleep 5s
+}
+
+# A single test "run".
+function run-test () {
+  local cnthreads=$1
+  local fhthreads=$2
+  local bs=$3
+  local lmem_mib=$4
+  local sleep_time=$5
+
+  # run test-one; time to start the application!
+  ssh $cn_control_sshname "zsh \$MIND_ROOT/apps/memcached/test-one.zsh $cnthreads $fhthreads $bs $lmem_mib $sleep_time"
+}
+
+cn=4
+fh=24
+bs=256
+
+lmem_mibs=(20480 18432 16384 14336 12288 10240 8192 8000)
+sleep_times=(200 100 80 50 20 5 0 0)
+
+# zip("lmem_mibs", "sleep_times")
+for (( i = 1; i <= $#lmem_mibs; i++ )); do 
+	local lmem_mib=$lmem_mibs[$i]
+	local sleep_time=$sleep_times[$i]
+
+	if (( fh + cn > 52 )); then
+		continue
+	fi
+
+	set-params $lmem_mib $sleep_time
+
+	run-test $cn $fh $bs $lmem_mib $sleep_time
+	fetch-test-logs "cn$cn-fh$fh-bs$bs-lmem_mib$lmem_mib-sleep$sleep_time-logs.1"
+done
---
+++ mage-kernel/apps/memcached/test-one.zsh
@@ -0,0 +1,30 @@
+#!/bin/zsh -e
+
+if [[ -z $MIND_ROOT ]]; then
+        echo '$MIND_ROOT not set!' >/dev/stderr
+        exit 1
+fi
+source $MIND_ROOT/scripts/config.sh
+cd $MIND_ROOT/apps/memcached
+
+if [[ $# -ne 5 ]]; then 
+	echo 'args: cn, fh, bs, lmem_mib, sleep_time!'
+	exit 1
+fi
+
+cn=$1
+fh=$2
+bs=$3
+local_mem_mib=$4
+sleep_time=$5
+
+log_root='/tmp/logs'
+output_log="$log_root/$fh.log"
+mkdir -p $log_root
+
+generate-pretest-logs $cn $fh $bs $local_mem_mib
+
+/usr/bin/time -v ./memcached -u root -t $fh -m20000 -v |& tee $output_log
+
+generate-posttest-logs $cn $fh $bs $local_mem_mib
+echo "test-one: done."
