8e593fd23cc03ceec94b7ade27005f73aedc8f94
Yash Lala <yashlala@gmail.com>
2025-08-16 00:03:42 +0200

    refactor: rename `metis` dir to `metis-mapreduce`.
    I keep getting confused.

---
+++ mage-kernel/apps/metis-mapreduce/run-benchmark.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh -e
+
+if [[ -z $MIND_ROOT ]]; then
+        echo '$MIND_ROOT not set!' >/dev/stderr
+        exit 1
+fi
+source $MIND_ROOT/scripts/config.sh
+cd $MIND_ROOT/apps/metis-mapreduce
+
+echo "Execute this on the VM host only!"
+
+
+# COMPILE THE BENCHMARK APPLICATION.
+ssh $cn_control_sshname \
+	'cd $MIND_ROOT/apps/metis-mapreduce/gapbs && make && if [[ -f pr ]]; then mv pr gapbs_pr; fi'
+
+# RUN THE TESTS
+echo "Running tests!"
+./kron-memscan.test-ssh.zsh
