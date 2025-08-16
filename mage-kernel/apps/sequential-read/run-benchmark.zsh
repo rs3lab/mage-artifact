5a7ebf74484dcb7ad6ffecafbb67f41c44251128
Yash Lala <yashlala@gmail.com>
2025-08-17 00:59:47 +0200

    util: refactor: remove dummy `test-ssh.zsh` wrapper script.

--- mage-kernel/apps/sequential-read/run-benchmark.zsh
+++ mage-kernel/apps/sequential-read/run-benchmark.zsh
@@ -7,11 +7,52 @@ fi
 source $MIND_ROOT/scripts/config.sh
 cd $MIND_ROOT/apps/sequential-read
 
-echo "Execute this on the VM host only!"
+echo 'Run this on the VM Host only!'
+
+# HELPER FUNCTIONS
+
+function set-params () {
+	local cn=$1
+	local bs=$2
+	local lmem_mib=$3
+	ssh $cn_control_sshname set-params 'test_mltthrd' $cn $bs $lmem_mib 0.5
+
+	manager cn allonly
+	sleep 1s
+}
+
+function run-test () {
+  local cnthreads=$1
+  local fhthreads=$2
+  local bs=$3
+  local lmem_mib=$4
+
+  # reset the cluster (updating the kernel as well)
+  reset-fbs
+  sleep 5s
+
+  # run test-one; time to start the application!
+  ssh $cn_control_sshname "zsh \$MIND_ROOT/apps/sequential-read/test-one.zsh $cnthreads $fhthreads $bs $lmem_mib"
+}
 
 
-# COMPILE THE BENCHMARK APPLICATION.
+# BEGIN TESTS: GENERATE FIGURE 14
 
-# Compile Benchmark + Kernel, RUN THE TESTS
-echo "Running tests!"
-./test-ssh.zsh
+# rebuild application
+local cn=4            # 4 eviction threads
+local bs=256          # 256 batch size
+local lmem_mib='1024' # 1 GiB DRAM Cache Size TODO(yash): confirm this
+
+set-params $cn $bs $lmem_mib
+
+# Run the tests!
+
+for fh in 1 2 4 8 16 32 40 48; do
+	if (( fh + cn > 52 )); then
+		continue
+	fi
+
+	run-test $cn $fh $bs $lmem_mib
+	# the "1" suffix indicates "run 1". Needed for our output scripts. 
+	fetch-test-logs "cn$cn-fh$fh-bs$bs-lmem_mib$lmem_mib-logs.1"
+done
